f8b4849b8a7d0342a9dbbac58d162a1d5950a957
JESUS ADRIAN LLANOS JIMENEZ <a17300148@live.ceti.mx>
2021-10-15 11:36:56 -0500

    Retroalimentacion del desafio 2

---
+++ Desafio3/Container.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+class Container {
+    constructor() {
+        this.path = 'database.txt';
+    }
+    async save (product){
+        let id;
+        let products;
+        products = await this.getAll();
+        if(products.length !== 0){
+            const lastProduct = products[products.length-1];
+            id = lastProduct.id + 1;
+        }else{
+            id = 1;
+        }
+        product.id = id;
+        products.push(product);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        return id;
+    }
+
+    async getById(id = 1){
+        const products = await this.getAll();
+        const product = products.find( product => product.id === id);
+        if(!product){
+            return null
+        }
+        return product;
+    }
+
+    async getAll(){
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            return JSON.parse(data);
+        }catch (e){
+            await fs.promises.writeFile(this.path, JSON.stringify([],null, 2));
+            return [];
+        }
+    }
+
+    async deleteById (id = 1){
+        let products = await this.getAll();
+        const deleteProduct = await this.getById(id);
+        const newProducts = products.filter( producto => producto.id !== deleteProduct.id );
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts, null, 2));
+    }
+
+    async deleteAll(){
+        await fs.promises.writeFile(this.path, JSON.stringify([], null, 2));
+    }
+}
+
+module.exports = Container;
